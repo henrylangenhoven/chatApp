2c40cfada2191ca26cdd64da7b820da02416501f
Henry Langenhoven <henrylangenhoven8@gmail.com>
2023-06-19 23:17:24 +0200

    added work in progress user with some defualts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {UserService} from "./user/user.service";
+import {User} from "./user/user.model";
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,10 @@ import {UserService} from "./user/user.service";
 })
 export class AppComponent {
   title = 'chatApp';
+  user?: User;
 
   constructor(private userService: UserService) { // TODO: remove this once user service is used somewhere else
+    this.user = this.userService.getOrCreateUser();
+    console.log(this.user)
   }
 }
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,13 +1,16 @@
 import {Injectable} from '@angular/core';
 import * as uuid from "uuid";
+import {User} from "./user.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private readonly userId: string;
+  private user?: User;
 
   constructor() {
+
     this.userId = this.getUserId();
   }
 
@@ -29,5 +32,26 @@ export class UserService {
     }
 
     return userIdFromStorage;
+  }
+
+  getOrCreateUser() {
+    if (!!this.user) {
+      return this.user;
+    }
+
+    // let name = prompt("Please enter your name") || "Harry Potter";
+    // let avatarUrl = prompt("Please enter your avatar url") || "https://bootdey.com/img/Content/avatar/avatar1.png";
+    let name = "Harry Potter";
+    let avatarUrl = "https://bootdey.com/img/Content/avatar/avatar1.png";
+
+    this.user = {
+      id: this.getUserId(),
+      name,
+      avatarUrl,
+      createdDate: new Date(),
+      isOnline: true
+    }
+
+    return this.user;
   }
 }
