f9406b593bc4b863d28c9d51d868954fd697a96f
Henry Langenhoven <henrylangenhoven8@gmail.com>
2023-10-10 11:41:24 +0200

    replaced verbose lifecycle stuff with `takeUntilDestroyed()`

--- src/app/chats/chat/chat.component.ts
+++ src/app/chats/chat/chat.component.ts
@@ -1,29 +1,20 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Contact } from '../../contacts/contact-item/contact.model';
 import { ContactService } from '../../contacts/contact.service';
-import { Subject, takeUntil } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
 })
-export class ChatComponent implements OnInit, OnDestroy {
-  private unsubscribe$: Subject<void> = new Subject<void>();
-
+export class ChatComponent {
   public contact: Contact = {} as Contact;
 
-  constructor(private contactService: ContactService) {}
-
-  ngOnInit(): void {
+  constructor(private contactService: ContactService) {
     this.contactService
       .getSelectedContact()
-      .pipe(takeUntil(this.unsubscribe$))
+      .pipe(takeUntilDestroyed())
       .subscribe((contact) => (this.contact = contact));
-  }
-
-  ngOnDestroy(): void {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
   }
 }
